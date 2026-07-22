2229b00e5f939aadcc727937e2de76745d6690e3
pratapabhay1 <68106890+pratapabhay1@users.noreply.github.com>
2026-07-22 23:06:01 +0530

    Implement database user retrieval in GetUser method
    Refactor GetUser method to retrieve user data from database.

--- VulnerableSite/Controllers/AppSecDemoController.cs
+++ VulnerableSite/Controllers/AppSecDemoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace VulnerableSite.Controllers
 {
@@ -6,10 +7,24 @@ namespace VulnerableSite.Controllers
     [Route("appsec")]
     public class AppSecDemoController : ControllerBase
     {
-        [HttpGet("hello")]
-        public IActionResult Hello()
+        private readonly string _connectionString =
+            "Server=(localdb)\\MSSQLLocalDB;Database=DemoDb;Trusted_Connection=True;";
+
+        [HttpGet("user")]
+        public IActionResult GetUser(string username)
         {
-            return Ok("AppSec Demo");
+            using var connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+
+            string query =
+                "SELECT * FROM Users WHERE Username = '" + username + "'";
+
+            using var command = new SqlCommand(query, connection);
+
+            using var reader = command.ExecuteReader();
+
+            return Ok("Executed");
         }
     }
 }
